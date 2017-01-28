c8a735ae0287d7251d232d1edf721673d990a4a5
dhelper <dror.helper@gmail.com>
2017-01-28 20:25:31 +0200

    Added Example Test

---
+++ StackOverflowHelperTests/UserFactoryTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using StackOverflowHelper;
+using StackOverflowHelper.Repository.Data;
+
+namespace StackOverflowHelperTests
+{
+    [TestFixture]
+    public class UserFactoryTests
+    {
+        [Test]
+        public void CreateAndInitializeViewModel_PreviousModelWasNullAndNoReputationGainedThatDay_ReputationChnageZero()
+        {
+            var user = new User
+            {
+                reputation_change_day = 0,
+                badge_counts = new BadgeCounts()
+            };
+
+            var userFactory = new UserFactory();
+
+            var result = userFactory.CreateAndInitializeViewModel(user, null);
+
+            Assert.That(result.ReputationChange, Is.EqualTo(0));
+        }
+    }
+}
